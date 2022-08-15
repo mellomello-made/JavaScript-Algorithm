e75d8d1f8627439d5b704046ea915b6a701cf3ad
Jungyun park <104332913+mellomello-made@users.noreply.github.com>
2022-08-15 12:31:08 +0900

    [level 1] Title: 모의고사, Time: 2.91 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -0,0 +1,32 @@
+function solution(answers) {
+    let score = [];
+    let result = [];
+    let person = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+    ];
+
+   
+    for (let i = 0; i < person.length; i++) {
+
+        score[i] = answers.filter(
+            (ele, idx) => ele === person[i][idx % person[i].length]
+        ).length;
+    }
+
+    let max = Math.max(...score);
+
+    if (score[0] === max){
+       result.push(1);
+    }
+    if (score[1] === max){
+       result.push(2);
+    }
+    if (score[2] === max){
+       result.push(3);
+    }
+    return result;
+}
+     
+
