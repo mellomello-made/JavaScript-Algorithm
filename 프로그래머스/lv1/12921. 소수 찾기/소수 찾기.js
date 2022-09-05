7ee22f1632c0f01549ae5cbf7204dd0a8adb6193
Jungyun park <104332913+mellomello-made@users.noreply.github.com>
2022-09-05 20:49:04 +0900

    [level 1] Title: 소수 찾기, Time: 60.82 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,36 +1,32 @@
 function solution(n) {
- 
-   let arr = [];
     
-    for(let i = 2; i <= n ; i++) {
-        arr[i] = i;
-    }
+    let answer = [];
+    let count = 0;
     
-    for(let i = 2; i <= n; i++) {
+     for(let i = 0 ; i < n + 1 ; i++){
+       answer.push(true);
+     }
     
-        if (arr[i] === 0){
-           
-        continue;
-            
-        }
-        
-        for(let j = i*2; j <= n; j += i) {
-            arr[j] = 0;
+    for(let i = 2 ; i * i <= n ; i++){
+        if (answer[i]) {
+            for (let j = i * i; j <= n ; j+= i){
+                answer[j] = false;
+            }
         }
     }
-
-    return arr.filter(e => e !== 0).length;
+    answer.splice(0, 2, false, false);
+    for(let i = 0; i < answer.length; i++){
+        if(answer[i]) count ++;
+    }
+    return count ++;
+    
 }
+    
+    
 
-// 1. 2부터 N 사이에 있는 소수를 찾는다. 0, 1은 제외한다. 
-// 2. 인덱스 2부터 반복문 돈다.
-// 3. 0이면 다시 다음 반복문을 돈다.
-// 4. 각 인덱스의 배수들을 0으로 지정한다.
-// 5. filter로 0이 아닌 수들의 갯수를 리턴한다.
-
-//첫번째 시도한 방식
 // 1. 1부터 N 사이에 있는 소수를 찾는다.
 // 2. 1은 소수에서 제외하고, 2부터 소수 담는 배열에 넣는다.
 // 3. 나눠지는 수가 있는지 반복문으로 찾는다. 
 // 4. 나눠지면 false , 나눠지는 수가 없는 소수의 경우이면 배열에 push한다.
 // 5. 배열의 원소 갯수를 반환한다.
+
