da73d0384d156145d248e1f697856cd99bddaad4
Jungyun park <104332913+mellomello-made@users.noreply.github.com>
2022-09-12 23:55:15 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 30.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,11 @@
+function solution(seoul) {
+   //배열의 모든 요소를 처음부터 특정 요소 'kim'을 발견할 때 까지 차례대로 검색한다.
+   //선형검색
+   // 대문자 'K', ~있다. 온점 금지
+    
+    //indexOf 메서드로 seoul 배열에 'Kim' 요소가  존재하는지 확인한다.
+    //요소가 존재하는 인덱스를 result 변수에 할당하여 출력한다.
+     let result = seoul.indexOf('Kim')     
+           return `김서방은 ${result}에 있다`
+     
+}
